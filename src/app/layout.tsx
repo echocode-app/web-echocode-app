31a320d78347cf68a7f2a5d0f4da01e3f5ad855b
BorovChe <roman2000borov@gmail.com>
2026-02-17 16:03:39 +0200

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-import { poppins, wadik, inter } from '@/styles/fonts/fonts';
+import { poppins, wadik, inter, workSans } from '@/styles/fonts/fonts';
 
 import './globals.css';
 import Header from '@/components/layout/Header';
@@ -20,12 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} ${inter.variable} ${wadik.variable} antialiased relative`}
+        className={`${poppins.variable} ${inter.variable} ${wadik.variable} ${workSans.variable} antialiased relative`}
       >
         <Header />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
         <SpeedInsights />
       </body>
